d7d1d815e6d3cd2deb8c0a688c467771e1104511
Binyamin Yawitz <biny1000@gmail.com>
2024-02-21 20:20:25 -0500

    [🖥️] - Initializing Dashboard with Vue 3

---
+++ apps/dashboard/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+</script>
+
+<template>
+  <h1>Welcome to linkos</h1>
+
+  <RouterView/>
+</template>
---
+++ apps/dashboard/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    Main page
+  </main>
+</template>
