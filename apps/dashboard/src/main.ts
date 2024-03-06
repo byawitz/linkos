c27a5f44d6bbe86ec59a5295fc70ebfdda5c890a
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-03-05 22:16:26 -0500

    [🖥️] - Matching Sweetalert colors
    Signed-off-by: Binyamin Yawitz <316103+byawitz@users.noreply.github.com>

--- apps/dashboard/src/main.ts
+++ apps/dashboard/src/main.ts
@@ -8,7 +8,6 @@ import App from './App.vue';
 import router from './router';
 import { useUserStore } from '@/stores/user';
 import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp(App);
 
