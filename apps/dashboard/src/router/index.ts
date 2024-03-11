b96cec54b83ebde77ff90c1ad43a5ee63767ef4b
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-03-11 17:01:16 -0400

    [🖥️] - Dashboard refactoring.
    Signed-off-by: Binyamin Yawitz <316103+byawitz@users.noreply.github.com>

--- apps/dashboard/src/router/index.ts
+++ apps/dashboard/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import ForgotView from '@/views/ForgotView.vue';
--- apps/dashboard/src/views/ForgotView.vue
+++ apps/dashboard/src/views/ForgotView.vue
@@ -2,7 +2,7 @@
   <Page :is-center="true">
     <Container :is-tight="true" class="py-4">
       <TextCenter class="mb-4">
-        <img src="@/assets/images/linkos-full.png" width="110" height="32" alt="Likos" class="navbar-brand-image">
+        <img src="@/assets/images/linkos-full.png" width="110" height="32" alt="Likos" class="navbar-brand-image" />
       </TextCenter>
 
       <Card card-size="card-md">
@@ -11,8 +11,8 @@
 
           <form autocomplete="off">
             <div class="mb-3">
-              <FormLabel label="Email Address"/>
-              <input type="email" v-model="state.email" class="form-control" placeholder="your@email.com" autocomplete="off">
+              <FormLabel label="Email Address" />
+              <input type="email" v-model="state.email" class="form-control" placeholder="your@email.com" autocomplete="off" />
             </div>
 
             <div class="form-footer">
@@ -23,39 +23,37 @@
       </Card>
 
       <div class="text-center text-secondary mt-3">
-        Forget it, <RouterLink to="/login">send me back</RouterLink> to the sign in screen.
+        Forget it,
+        <RouterLink to="/login">send me back</RouterLink>
+        to the sign in screen.
       </div>
     </Container>
   </Page>
 </template>
 
 <script setup lang="ts">
-import Page from "@/components/layouts/Page.vue";
-import Container from "@/components/layouts/Container.vue";
-import {onMounted, onUnmounted, reactive} from "vue";
-import DomHelper from "@/heplers/DomHelper";
-import TextCenter from "@/components/text/TextCenter.vue";
-import HrText from "@/components/text/HrText.vue";
-import Card from "@/components/layouts/Card.vue";
-import CardBody from "@/components/layouts/CardBody.vue";
-import FormLabel from "@/components/form/FormLabel.vue";
-import InputGroup from "@/components/form/InputGroup.vue";
-import InputGroupText from "@/components/form/InputGroupText.vue";
-import {IconEye, IconEyeOff, IconBrandGithub, IconBrandTwitter} from '@tabler/icons-vue';
-import Button from "@/components/form/Button.vue";
+import Page from '@/components/layouts/Page.vue';
+import Container from '@/components/layouts/Container.vue';
+import { onMounted, onUnmounted, reactive } from 'vue';
+import DomHelper from '@/heplers/DomHelper';
+import TextCenter from '@/components/text/TextCenter.vue';
+import Card from '@/components/layouts/Card.vue';
+import CardBody from '@/components/layouts/CardBody.vue';
+import FormLabel from '@/components/form/FormLabel.vue';
+import Button from '@/components/form/Button.vue';
 
 onMounted(() => {
-  DomHelper.addClassToApp('page', 'page-center');
+  DomHelper.addClassToID('app', 'page', 'page-center');
 });
 
 onUnmounted(() => {
-  DomHelper.removeClassFromApp('page', 'page-center');
+  DomHelper.removeClassFromID('app', 'page', 'page-center');
 });
 
 const state = reactive({
-  email       : '',
-  password    : '',
+  email: '',
+  password: '',
   showPassword: false,
-  rememberMe  : false,
-})
+  rememberMe: false
+});
 </script>
